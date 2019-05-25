e883f267c0b449fb7acce63e5c9594eb11d5589d
Chriswarkidd <chriswarkidd@gmail.com>
2019-05-24 21:59:30 -0400

    Updates

---
+++ precache-manifest.1c23d93197f02562cfc3d30e76b47d13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6daa3f54bfb0ba9df4584c2bc7b864c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "38ec8660379154a42a8c",
+    "url": "/static/css/main.45d17070.chunk.css"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "38ec8660379154a42a8c",
+    "url": "/static/js/main.a2f08dbe.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
